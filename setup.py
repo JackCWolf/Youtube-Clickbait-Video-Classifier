8fcb97f1890be122b43182a033fa3f04a6ec5f5a
Jack Wolfgramm <jackvwolfgramm@gmail.com>
2022-08-21 23:31:25 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Create a model that can classify clickbait videos based on the thumbnail',
+    author='Jack Wolfgramm',
+    license='MIT',
+)
